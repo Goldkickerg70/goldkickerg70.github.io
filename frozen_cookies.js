28bee8fbc12a4c8fa6b85f7922f161f16e43916a
Goldkickerg70 <44851876+Goldkickerg70@users.noreply.github.com>
2025-11-04 20:44:05 -0500

    Update frozen_cookies.js

--- frozen_cookies.js
+++ frozen_cookies.js
@@ -20,7 +20,7 @@ var scriptElement =
 var baseUrl =
     scriptElement !== null
         ? scriptElement.getAttribute("src").replace(/\/frozen_cookies\.js$/, "")
-        : "https://goldkickerg70.github.io/";
+        : "https://github.erbkaiser.com/FrozenCookies/";
 var FrozenCookies = {
     baseUrl: baseUrl,
     branch: "erb-",
